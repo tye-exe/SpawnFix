e7b5e318f871e5e7335c35ae4454423f02e600f4
Mapty231 <tye.exe@gmail.com>
2023-11-15 22:53:16 +0000

    Sends link to config help on start up.

--- src/main/java/me/tye/spawnfix/SpawnFix.java
+++ src/main/java/me/tye/spawnfix/SpawnFix.java
@@ -2,19 +2,25 @@ package me.tye.spawnfix;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class SpawnFix extends JavaPlugin {
 
 @Override
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
-        getServer().getPluginManager().registerEvents(new PlayerLeave(), this);
-        getServer().getPluginManager().registerEvents(new PlayerRespawn(), this);
+public void onEnable() {
 
-        saveDefaultConfig();
-    }
+    getLogger().log(Level.INFO, "See the readme on github for config help:");
+    getLogger().log(Level.INFO, "https://github.com/Mapty231/SpawnFix/blob/master/README.md");
 
-    @Override
-    public void onDisable() {
+    getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+    getServer().getPluginManager().registerEvents(new PlayerLeave(), this);
+    getServer().getPluginManager().registerEvents(new PlayerRespawn(), this);
 
-    }
+    saveDefaultConfig();
+}
+
+@Override
+public void onDisable() {
+
+}
 }
