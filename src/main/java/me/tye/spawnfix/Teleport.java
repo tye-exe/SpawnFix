bb6d07e6ce6bf9de8c05d631aee8089016c3d50f
Mapty231 <tye.exe@gmail.com>
2023-12-02 19:12:20 +0000

    Error message for parsing will only be shown once

--- src/main/java/me/tye/spawnfix/Teleport.java
+++ src/main/java/me/tye/spawnfix/Teleport.java
@@ -20,6 +20,7 @@ private final Player player;
 private final Location location;
 
 private int timesTeleported = 1;
+private int retryLimit = 10;
 
 /**
  A runnable object that teleports the given player to the given location the amount of times specified by "teleport.times" in the config.
@@ -29,18 +30,17 @@ private int timesTeleported = 1;
 public Teleport(@NonNull Player player, @NonNull Location location) {
   this.player = player;
   this.location = location;
-}
 
-@Override
-public void run() {
-  int retryLimit = 10;
-
+  //Gets the user config for the max amount of teleport times.
   try {
     retryLimit = Integer.parseInt(get("teleport.times"));
   } catch (NumberFormatException e) {
     plugin.getLogger().log(Level.WARNING, "Unable to parse the max amount of teleport times, defaulting to 10.");
   }
+}
 
+@Override
+public void run() {
   if (timesTeleported > retryLimit) {
     runningTasks.get(player.getUniqueId()).cancel();
     return;
